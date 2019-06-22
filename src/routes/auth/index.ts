e9a09e5974e693ee2e58b4236e7673691606693b
bor1bori <bor1boridv@gmail.com>
2019-06-23 01:20:54 +0900

    add cors

--- src/routes/auth/index.ts
+++ src/routes/auth/index.ts
@@ -47,9 +47,7 @@ router.post('/register', (req: any, res: any) => {
         if (user === null) {
           UserModel.create({id: receiveID, pw: hashPassword, mem_power: req.body.mem_power});
           console.log(2);
-          res.status(200).type('JSON');
-          res.write(JSON.stringify({success: true}));
-          res.end;
+          res.status(200).json({success: true});
         } else {
           res.status(202).json({error: 'DuplicatedID'});
         }
