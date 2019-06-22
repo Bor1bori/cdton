69aef9ee35258f65f5c4f31914edfec2403ce656
bor1bori <bor1boridv@gmail.com>
2019-06-22 21:11:33 +0900

    add cors

--- src/app.ts
+++ src/app.ts
@@ -17,9 +17,10 @@ const logger = createLogger({
 });
 const app = express();
 
-app.all('/*', function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+app.all('/*', (req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', '*');
+  res.header('Access-Control-Allow-Headers', '*');
   next();
 });
 
