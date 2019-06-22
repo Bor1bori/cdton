599c417cbf031b52724e80359a358cf9581172e5
bor1bori <bor1boridv@gmail.com>
2019-06-22 21:36:28 +0900

    add cors

--- src/app.ts
+++ src/app.ts
@@ -52,9 +52,9 @@ mongo(); // mongo DB ON
 // app.use(cors());
 
 app.options('*', (req, res) => {
-  res.header('Access-Control-Allow-Origin', 'http://127.0.0.1:5500');
-  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+  res.append('Access-Control-Allow-Origin', ['*']);
+  res.append('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+  res.append('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
   console.log(1);
   res.status(200).json({dohun: 'hi'});
 });
